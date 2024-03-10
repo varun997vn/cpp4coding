2e401ad118cae3c7a06defb03f66ce4926916430
varun997vn <varun997vn@outlook.com>
2024-03-10 23:42:06 +0800

    add code: move

---
+++ algorithms/move.cpp
@@ -0,0 +1,28 @@
+// Note: std::move in algorithm library is not same as std::move in utilities library
+// This moves the elements from one range ot another and the elements in moved_from need not remain same after move.
+// This is similar to copy but more efficient in large datastructures.
+
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include "utils.hpp"
+
+int main()
+{
+    std::vector<int> v1{1, 2, 3, 4, 5, 6};
+    std::vector<int> v2(6);
+
+    std::cout << "Moved From vector: ";
+    println(v1);
+
+    std::cout << "Perform Move\n";
+    std::move(v1.begin(), v1.end(), v2.begin());
+
+    std::cout << "Moved From Vector: ";
+    println(v1);
+
+    std::cout << "Moved to Vector: ";
+    println(v2);
+
+    return 0;
+}
